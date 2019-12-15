00f472d3bc3daedfc88e3d9da6bb8f202550ded1
Miroslav Dinev <miroslavdinev@yahoo.com>
2019-12-15 13:38:48 +0200

    Added Code First

---
+++ C# DB - EF Core/Exercises/Code First/CodeFirst/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<PatientMedicament> PatientsMedicaments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            this.ConfigurePatientEntity(modelBuilder);
+            this.ConfigureVisitationEntity(modelBuilder);
+            this.ConfigureDiagnoseEntity(modelBuilder);
+            this.ConfigureMedicamentEntity(modelBuilder);
+            this.ConfigurePatientMedicamentEntity(modelBuilder);
+            this.ConfigureDoctorEntity(modelBuilder);
+        }
+
+        private void ConfigureDoctorEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>()
+                .HasKey(d => d.DoctorId);
+
+            modelBuilder.Entity<Doctor>()
+                .Property(n => n.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder.Entity<Doctor>()
+                .Property(s => s.Specialty)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(v => v.Visitations)
+                .WithOne(d => d.Doctor);
+        }
+
+        private void ConfigurePatientMedicamentEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatientMedicament>()
+                .HasKey(x => new { x.PatientId, x.MedicamentId });
+
+            modelBuilder.Entity<PatientMedicament>()
+                .HasOne(p => p.Patient)
+                .WithMany(ps => ps.Prescriptions)
+                .HasForeignKey(p => p.PatientId);
+
+            modelBuilder.Entity<PatientMedicament>()
+                .HasOne(m => m.Medicament)
+                .WithMany(p => p.Prescriptions)
+                .HasForeignKey(m => m.MedicamentId);
+        }
+
+        private void ConfigureMedicamentEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicament>()
+                .HasKey(m => m.MedicamentId);
+
+            modelBuilder.Entity<Medicament>()
+                .Property(n => n.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+        }
+
+        private void ConfigureDiagnoseEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Diagnose>()
+                .HasKey(d => d.DiagnoseId);
+
+            modelBuilder.Entity<Diagnose>()
+                .Property(n => n.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder.Entity<Diagnose>()
+                .Property(c => c.Comments)
+                .HasMaxLength(250)
+                .IsUnicode();
+        }
+
+        private void ConfigureVisitationEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Visitation>()
+                .HasKey(v => v.VisitationId);
+
+            modelBuilder.Entity<Visitation>()
+                .Property(c => c.Comments)
+                .HasMaxLength(250)
+                .IsUnicode();
+                
+        }
+
+        private void ConfigurePatientEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>()
+                .HasKey(p => p.PatientId);
+
+            modelBuilder.Entity<Patient>()
+                .Property(f => f.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder.Entity<Patient>()
+                .Property(l => l.LastName)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder.Entity<Patient>()
+                .Property(a => a.Address)
+                .HasMaxLength(250)
+                .IsUnicode();
+
+            modelBuilder.Entity<Patient>()
+                .Property(e => e.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(v => v.Visitations)
+                .WithOne(p => p.Patient);
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(d => d.Diagnoses)
+                .WithOne(p => p.Patient);
+           
+        }
+    }
+}
---
+++ C# DB - EF Core/Exercises/Code First/CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            this.ProductEntityCreator(modelBuilder);
+            this.CustomerEntityCreator(modelBuilder);
+            this.StoreEntityCreator(modelBuilder);
+            this.SaleEntityCreator(modelBuilder);
+        }
+
+        private void SaleEntityCreator(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasKey(s => s.SaleId);
+
+            modelBuilder.Entity<Sale>()
+                .Property(d => d.Date)
+                .HasDefaultValueSql("GETDATE()");
+
+        }
+
+        private void StoreEntityCreator(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Store>()
+                .HasKey(s => s.StoreId);
+
+            modelBuilder.Entity<Store>()
+                .Property(n => n.Name)
+                .HasMaxLength(80)
+                .IsUnicode();
+
+            modelBuilder.Entity<Store>()
+                .HasMany(s => s.Sales)
+                .WithOne(st => st.Store);
+        }
+
+        private void CustomerEntityCreator(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder.Entity<Customer>()
+                .Property(n => n.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(s => s.Sales)
+                .WithOne(c => c.Customer);
+        }
+
+        private void ProductEntityCreator(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasKey(x => x.ProductId);
+
+            modelBuilder.Entity<Product>()
+                .Property(n => n.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder.Entity<Product>()
+                .Property(d => d.Description)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(s => s.Sales)
+                .WithOne(p => p.Product);
+        }
+    }
+}
