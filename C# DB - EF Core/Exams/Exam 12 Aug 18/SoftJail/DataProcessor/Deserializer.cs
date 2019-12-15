f1c0b35ba28faaa685abd880353422105b74ee0d
Miroslav Dinev <miroslavdinev@yahoo.com>
2019-12-15 15:05:11 +0200

    Added Exam 12 Aug 18

---
+++ C# DB - EF Core/Exams/Exam 12 Aug 18/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,213 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using SoftJail.Data.Models.Enums;
+    using SoftJail.DataProcessor.ImportDto;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            var departmentsDTOs = JsonConvert.DeserializeObject<ImportDepartmentsDto[]>(jsonString);
+
+            var sb = new StringBuilder();
+
+            var departments = new List<Department>();
+
+            foreach (var departmentDto in departmentsDTOs)
+            {
+                if(!IsValid(departmentDto))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                var isValidCell = true;
+
+                foreach (var cellDto in departmentDto.Cells)
+                {
+                    if(!IsValid(cellDto))
+                    {
+                        sb.AppendLine("Invalid Data");
+                        isValidCell = false;
+                        break; ;
+                    }
+                }
+
+                if(!isValidCell)
+                {
+                    continue;
+                }
+
+                var department = new Department
+                {
+                    Name = departmentDto.Name
+                };
+
+                foreach (var cellDto in departmentDto.Cells)
+                {
+                    department.Cells.Add(new Cell
+                    {
+                        CellNumber = cellDto.CellNumber,
+                        HasWindow = cellDto.HasWindow
+                    });
+                }
+
+                departments.Add(department);
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+            }
+
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            var prisonersDTOs = JsonConvert.DeserializeObject<ImportPrisonersDto[]>(jsonString);
+
+            var sb = new StringBuilder();
+
+            var prisoners = new List<Prisoner>();
+
+            foreach (var prisonerDto in prisonersDTOs)
+            {
+                if(!IsValid(prisonerDto))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                bool isValidMail = true;
+
+                foreach (var mailDto in prisonerDto.Mails)
+                {
+                    if(!IsValid(mailDto))
+                    {
+                        isValidMail = false;
+                        sb.AppendLine("Invalid Data");
+                        break;
+                    }
+                }
+
+                if(!isValidMail)
+                {
+                    continue;
+                }
+
+                DateTime? releaseDate = null;
+
+                if(prisonerDto.ReleaseDate!=null)
+                {
+                    releaseDate = DateTime.ParseExact(prisonerDto.ReleaseDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                }
+
+                var prisoner = new Prisoner
+                {
+                    FullName = prisonerDto.FullName,
+                    Nickname = prisonerDto.Nickname,
+                    Age = prisonerDto.Age,
+                    IncarcerationDate = DateTime.ParseExact(prisonerDto.IncarcerationDate, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    ReleaseDate = releaseDate,
+                    Bail = prisonerDto.Bail,
+                    CellId = prisonerDto.CellId
+                };
+
+                foreach (var mailDto in prisonerDto.Mails)
+                {
+                    prisoner.Mails.Add(new Mail
+                    {
+                        Description = mailDto.Description,
+                        Sender = mailDto.Sender,
+                        Address = mailDto.Address
+                    });
+                }
+
+                prisoners.Add(prisoner);
+                sb.AppendLine($"Imported {prisoner.FullName} {prisoner.Age} years old");
+            }
+
+            context.Prisoners.AddRange(prisoners);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportOfficerDto[]), new XmlRootAttribute("Officers"));
+
+            var officersDTOs = (ImportOfficerDto[]) xmlSerializer.Deserialize(new StringReader(xmlString));
+
+            var sb = new StringBuilder();
+
+            var officers = new List<Officer>();
+
+            foreach (var officerDto in officersDTOs)
+            {
+                if(!IsValid(officerDto))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                bool isValidPosition = Enum.TryParse(officerDto.Position, out Position position);
+                bool isValidWeapon = Enum.TryParse(officerDto.Weapon, out Weapon weapon);
+
+                if(isValidPosition==false || isValidWeapon==false)
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                var officer = new Officer
+                {
+                    FullName = officerDto.Name,
+                    Salary = officerDto.Money,
+                    Position = position,
+                    Weapon = weapon,
+                    DepartmentId = officerDto.DepartmentId
+                };
+
+                foreach (var prisonerDto in officerDto.Prisoners)
+                {
+                    officer.OfficerPrisoners.Add(new OfficerPrisoner
+                    {
+                        PrisonerId = prisonerDto.Id,
+                        Officer = officer
+                    });
+                }
+
+                officers.Add(officer);
+                sb.AppendLine($"Imported {officer.FullName} ({officer.OfficerPrisoners.Count} prisoners)");
+            }
+
+            context.Officers.AddRange(officers);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object entity)
+        {
+            var validationContext = new ValidationContext(entity);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(entity, validationContext, validationResult, true);
+
+            return isValid;
+        }
+    }
+}
---
+++ C# DB - EF Core/Exams/Exam 12 Aug 18/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,90 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    CellNumber = x.Cell.CellNumber,
+                    Officers = x.PrisonerOfficers
+                    .Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name
+                    })
+                    .OrderBy(n => n.OfficerName)
+                    .ToArray(),
+                    TotalOfficerSalary = x.PrisonerOfficers.Sum(p => p.Officer.Salary)
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(i => i.Id)
+                .ToArray();
+
+            var jsonResult = JsonConvert.SerializeObject(prisoners, Newtonsoft.Json.Formatting.Indented);
+
+            return jsonResult;
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            var prisonerNames = prisonersNames.Split(",").ToList();
+
+            var prisoners = context.Prisoners
+                .Where(p => prisonerNames.Contains(p.FullName))
+                .Select(x => new ExportPrisonerDto
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    IncarcerationDate = x.IncarcerationDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                    EncryptedMessages = x.Mails
+                    .Select(m => new ExportMessagesDto
+                    {
+                        Description = ReverseText(m.Description)
+                    })
+                    .ToArray()
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportPrisonerDto[]), new XmlRootAttribute("Prisoners"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[]
+            {
+                XmlQualifiedName.Empty
+            });
+
+            xmlSerializer.Serialize(new StringWriter(sb), prisoners, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string ReverseText(string description)
+        {
+            var descriptionAsArray = description.ToCharArray();
+
+            Array.Reverse(descriptionAsArray);
+
+            return string.Join("", descriptionAsArray);
+        }
+    }
+}
